2ac0840dec21c2af696b9bb0f26022700351cd39
Anders Haahr <anders.haahr@groupm.com>
2015-01-05 11:16:20 +0100

    Boilerplate for the RoboModel

--- app/src/main/java/net/haahr/jaywayrobot/mvc/RoboModel.java
+++ app/src/main/java/net/haahr/jaywayrobot/mvc/RoboModel.java
@@ -11,10 +11,15 @@ import java.util.ArrayList;
  */
 public class RoboModel {
 
-    /**
-     * The list of instructions. The list has inner type String, but only the values 'F', 'L' and 'R' are valid
-     */
     private ArrayList<String> instructions;
+
+    private RoboPoint startPosition;
+
+    private RoboVector2D startDirection;
+
+    private int sizeX;
+
+    private int sizeY;
 
 
     public RoboModel(){
@@ -30,32 +35,76 @@ public class RoboModel {
         instructions.add(instruction);
     }
 
+
     /**
-     * @param startPosition The starting position for the robot
+     * @return the instructions list for the robot
+     */
+    public ArrayList<String> getInstructions(){
+        return this.instructions;
+    }
+
+
+    /**
+     * @param startPosition The starting position for the robot in the grid
      */
     public void setStartPosition(RoboPoint startPosition){
-
+        this.startPosition = startPosition;
     }
+
+
+    /**
+     * @return the starting position for the robot in the grid
+     */
+    public RoboPoint getStartPosition(){
+        return this.startPosition;
+    }
+
 
     /**
      * @param startDirection The starting direction for the robot, where (1, 0) is east, (0, 1) is south, (-1, 0) is east and (0, -1) is north
      */
     public void setStartDirection(RoboVector2D startDirection){
-
+        this.startDirection = startDirection;
     }
+
+
+    /**
+     * @return the starting direction for the robot, where (1, 0) is east, (0, 1) is south, (-1, 0) is east and (0, -1) is north
+     */
+    public RoboVector2D getStartDirection(){
+        return this.startDirection;
+    }
+
 
     /**
      * @param sizeX The horizontal size of the grid
      */
     public void setSizeX(int sizeX){
-
+        this.sizeX = sizeX;
     }
+
+
+    /**
+     * @return The horizontal size of the grid
+     */
+    public int getSizeX(){
+        return this.sizeX;
+    }
+
 
     /**
      * @param sizeY The vertical size of the grid
     */
     public void setSizeY(int sizeY){
-        
+        this.sizeY = sizeY;
+    }
+
+
+    /**
+     * @return The vertical size of the grid
+     */
+    public int getSizeY(){
+        return this.sizeY;
     }
 
 }
