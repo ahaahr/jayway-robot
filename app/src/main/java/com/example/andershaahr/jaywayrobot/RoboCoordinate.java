a1468d259e6b57f7e9cadcbf18ae95878e3e7126
Anders Haahr <anders.haahr@groupm.com>
2015-01-05 04:31:27 +0100

    First commit

---
+++ app/src/main/java/com/example/andershaahr/jaywayrobot/RoboCoordinate.java
@@ -0,0 +1,15 @@
+package com.example.andershaahr.jaywayrobot;
+
+import android.graphics.Point;
+
+/**
+ * Created by anders.haahr on 05/01/15.
+ * A coordinate system for the robot to nivigate in
+ */
+public class RoboCoordinate extends Point {
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/andershaahr/jaywayrobot/RoboVector2D.java
@@ -0,0 +1,11 @@
+package com.example.andershaahr.jaywayrobot;
+
+/**
+ * Created by anders.haahr on 05/01/15.
+ * A vector representing a direction for the robot to move
+ */
+public class RoboVector2D {
+
+    
+
+}
