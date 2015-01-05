cde44bdd3d82323b93acc01e027f007549e86afc
Anders Haahr <anders.haahr@groupm.com>
2015-01-05 04:50:23 +0100

    Hide ActionBar on startup

--- app/src/main/java/com/example/andershaahr/jaywayrobot/RoboActivity.java
+++ app/src/main/java/com/example/andershaahr/jaywayrobot/RoboActivity.java
@@ -1,40 +1,24 @@
 package com.example.andershaahr.jaywayrobot;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ActionBar;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class RoboActivity extends ActionBarActivity {
+public class RoboActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_robo);
 
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_robo, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        //since we do not need the action bar, hide it
+        ActionBar actionBar = this.getActionBar();
+        if(actionBar != null){
+            actionBar.hide();
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+
 }
