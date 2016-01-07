1f74fa11b2d1f7833644456532e1e336a00563a6
Anders Haahr <anders.haahr@groupm.com>
2016-01-07 18:57:35 +0100

    Added more tests for the RoboState class.

--- app/src/test/java/net/haahr/jaywayrobot/robot/RoboStateTest.java
+++ app/src/test/java/net/haahr/jaywayrobot/robot/RoboStateTest.java
@@ -15,24 +15,182 @@ import static org.junit.Assert.*;
  */
 public class RoboStateTest {
 
-    private RoboModel roboModel;
+    private RoboModel roboModel1x1;
+    private RoboModel roboModel5x5;
     private RoboState roboState;
 
     @Before
     public void setUp() throws Exception {
-        roboModel = new RoboModel();
-        roboModel.addInstruction("F");
-        roboModel.setStartPosition(new RoboPoint(0, 0));
-        roboModel.setStartDirection(new RoboVector2D(0, 1));
-        roboModel.setSizeX(10);
-        roboModel.setSizeY(10);
+        roboModel1x1 = new RoboModel();
+        roboModel1x1.setStartPosition(new RoboPoint(0, 0));
+        roboModel1x1.setStartDirection(new RoboVector2D(1, 0));
+        roboModel1x1.setSizeX(1);
+        roboModel1x1.setSizeY(1);
+
+        roboModel5x5 = new RoboModel();
+        roboModel5x5.setStartPosition(new RoboPoint(1, 1));
+        roboModel5x5.setStartDirection(new RoboVector2D(1, 0));
+        roboModel5x5.setSizeX(5);
+        roboModel5x5.setSizeY(5);
 
         roboState = new RoboState();
     }
 
     @Test
-    public void testHorizontalMovementTest1(){
-        RoboPoint result = roboState.start(roboModel);
-        assertEquals(1, result.x);
+    public void test1x1Grid(){
+        roboModel1x1.addInstruction("F");
+        RoboPoint result = roboState.start(roboModel1x1);
+        assertEquals(0, result.x);
     }
+
+    @Test
+    public void testNoMovement(){
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(1, result.x);
+        assertEquals(1, result.y);
+    }
+
+    @Test
+    public void testMovement(){
+        roboModel5x5.addInstruction("F");
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(2, result.x);
+        assertEquals(1, result.y);
+    }
+
+    @Test
+    public void testRightTurn(){
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("F");
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(1, result.x);
+        assertEquals(2, result.y);
+    }
+
+    @Test
+    public void testLeftTurn(){
+        roboModel5x5.addInstruction("L");
+        roboModel5x5.addInstruction("F");
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(1, result.x);
+        assertEquals(0, result.y);
+    }
+
+    @Test
+    public void testLoop(){
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("R");
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(1, result.x);
+        assertEquals(1, result.y);
+    }
+
+    @Test
+    public void testNorthBound(){
+        roboModel5x5.addInstruction("L");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(0, result.y);
+    }
+
+    @Test
+    public void testEastBound(){
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(4, result.x);
+    }
+
+    @Test
+    public void testSouthBound(){
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(4, result.y);
+    }
+
+    @Test
+    public void testWestBound(){
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(0, result.x);
+    }
+
+    @Test
+    public void testMultipleBounds(){
+        //hit west bound, then north bound, then south bound
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("L");
+        roboModel5x5.addInstruction("L");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(0, result.x);
+        assertEquals(4, result.y);
+    }
+
+    @Test
+    public void testEachCornerAndGotoCenter(){
+        //hit each corner, then goto center
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("R");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("L");
+        roboModel5x5.addInstruction("L");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("L");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("L");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("L");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("L");
+        roboModel5x5.addInstruction("F");
+        roboModel5x5.addInstruction("F");
+        RoboPoint result = roboState.start(roboModel5x5);
+        assertEquals(2, result.x);
+        assertEquals(2, result.y);
+    }
+
+
 }
